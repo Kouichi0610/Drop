350ab324aeeec311a149397f09948ed34896cc3d
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2021-04-05 15:03:57 +0900

    左右移動補正

--- Assets/Scripts/View/DropGems.cs
+++ Assets/Scripts/View/DropGems.cs
@@ -16,6 +16,9 @@ namespace View {
         int startY = 0;
         FieldSize size;
 
+        List<Gem> gems = new List<Gem>();
+
+
         public void SetUp(IGemsFactory factory, FieldSize size) {
             this.factory = factory;
             this.startX = size.Width/2;
@@ -30,6 +33,7 @@ namespace View {
                 g.transform.SetParent(this.transform);
                 g.transform.name = "Gem_" + i;
                 g.transform.position = new Vector3(0, i, 0);
+                this.gems.Add(g);
                 i++;
             }
             transform.position = new Vector3(startX, startY, 0);
@@ -48,19 +52,16 @@ namespace View {
         }
 
         void Update() {
-            /*
-            if (Input.GetButtonDown("Left")) {
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
                 Left();
-            } else if (Input.GetButtonDown("Right")) {
+            } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
                 Right();
-            } else if (Input.GetButtonDown("Down")) {
+            } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
                 DecideDrop();
-            } else if (Input.GetButtonDown("Space")) {
+            } else if (Input.GetKeyDown(KeyCode.Space)) {
                 Rotate();
             }
-            */
         }
-
 
         // 設置判定
         bool IsGround() {
@@ -68,22 +69,44 @@ namespace View {
         }
 
         void DecideDrop() {
-            Debug.Log("Decide.");
         }
 
         void Left() {
             var current = transform.position;
             var next = new Vector3(current.x-1, current.y, current.z);
             transform.position = next;
+            HorizontalClip();
         }
+
         void Right() {
             var current = transform.position;
             var next = new Vector3(current.x+1, current.y, current.z);
             transform.position = next;
+            HorizontalClip();
         }
 
         void Rotate() {
-            Debug.Log("Rotate.");
+            var next = Quaternion.AngleAxis(-90, Vector3.forward);
+            transform.rotation = transform.rotation * next;
+            HorizontalClip();
+        }
+
+        void HorizontalClip() {
+            var left = 0;
+            var right = size.Width;
+            float append = 0;
+
+            foreach (var g in gems) {
+                var x = g.transform.position.x;
+                if (x < left) {
+                    append = left - x;
+                } else if (x > right) {
+                    append = right - x;
+                }
+            }
+            var next = transform.position;
+            next.x = next.x + append;
+            transform.position = next;
         }
 
         void Start()
