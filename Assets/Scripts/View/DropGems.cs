a0e1b490f04d1852ef11091278fc52a8e88ae10a
Kouichi0610 <53707533+Kouichi0610@users.noreply.github.com>
2021-04-05 14:08:21 +0900

    バージョン更新

--- Assets/Scripts/View/DropGems.cs
+++ Assets/Scripts/View/DropGems.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Model;
+using Model.Rules;
 using UniRx;
 
 namespace View {
@@ -13,11 +14,13 @@ namespace View {
         IGemsFactory factory = null;
         int startX = 0;
         int startY = 0;
+        FieldSize size;
 
-        public void SetUp(IGemsFactory factory, Model.Rules.FieldSize size) {
+        public void SetUp(IGemsFactory factory, FieldSize size) {
             this.factory = factory;
             this.startX = size.Width/2;
             this.startY = size.Height;
+            this.size = size;
         }
 
         public void Initialize(params Gems[] gems) {
@@ -35,13 +38,52 @@ namespace View {
 
         IEnumerator Drop() {
             // TODO:落下判定
-            while (transform.position.y > 0) {
+            while (!IsGround()) {
                 yield return new WaitForSeconds(1);
                 var current = transform.position;
                 var next = new Vector3(current.x, current.y-1, current.z);
                 transform.position = next;
             }
             yield return null;
+        }
+
+        void Update() {
+            /*
+            if (Input.GetButtonDown("Left")) {
+                Left();
+            } else if (Input.GetButtonDown("Right")) {
+                Right();
+            } else if (Input.GetButtonDown("Down")) {
+                DecideDrop();
+            } else if (Input.GetButtonDown("Space")) {
+                Rotate();
+            }
+            */
+        }
+
+
+        // 設置判定
+        bool IsGround() {
+            return transform.position.y <= 0;
+        }
+
+        void DecideDrop() {
+            Debug.Log("Decide.");
+        }
+
+        void Left() {
+            var current = transform.position;
+            var next = new Vector3(current.x-1, current.y, current.z);
+            transform.position = next;
+        }
+        void Right() {
+            var current = transform.position;
+            var next = new Vector3(current.x+1, current.y, current.z);
+            transform.position = next;
+        }
+
+        void Rotate() {
+            Debug.Log("Rotate.");
         }
 
         void Start()
